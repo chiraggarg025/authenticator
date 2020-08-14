dd10dd3ea5378c3f278f14a6b1fa8f6de5c81541
chanakya.garg <chiraggarg025@gmail.com>
2020-08-14 09:55:15 +0530

    configure databse and add passport local strategy

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+const session = require('express-session');
+
+
+
+
+
+
 app.get('/',function(req,res){
     return res.end('<h1>HI</h1>');
 })
