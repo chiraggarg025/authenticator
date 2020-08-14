82f118a67077cc4409ffe35a530707f81efbffa8
chanakya.garg <chiraggarg025@gmail.com>
2020-08-14 09:39:55 +0530

    add user model and npm packages like passport

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.get('/',function(req,res){
     return res.end('<h1>HI</h1>');
 })
 
-app.listen(port,function(req,res){
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in starting the server : ${err}`);
+    }
     console.log(`Server is running on port: ${port}`);
 })
